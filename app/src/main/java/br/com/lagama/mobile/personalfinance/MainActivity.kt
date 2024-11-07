add229b0ca0ae16111dea06de4d8fc7164f86e12
Alexandre Ribeiro <xanderib@gmail.com>
2024-11-07 17:47:50 -0300

    Implementação da estrutura base com fragments

--- app/src/main/java/br/com/lagama/mobile/personalfinance/MainActivity.kt
+++ app/src/main/java/br/com/lagama/mobile/personalfinance/MainActivity.kt
@@ -1,65 +1,44 @@
 package br.com.lagama.mobile.personalfinance
 
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var icons: List<ImageView>
-    private lateinit var texts: List<TextView>
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val backgroundGradient = GradientDrawable(
-            GradientDrawable.Orientation.TL_BR,
-            intArrayOf(
-                ContextCompat.getColor(this, R.color.start_color),
-                ContextCompat.getColor(this, R.color.end_color)
-            )
-        )
-        val rootView = findViewById<RelativeLayout>(R.id.root_layout)
-        rootView.background = backgroundGradient
+        if (savedInstanceState == null) {
+            super.updateIconColors(icons[IDX_AGENDA])
+            loadFragment(AgendaFragment())
+        }
 
-        icons = listOf(
-            findViewById<ImageView>(R.id.icon_groups),
-            findViewById<ImageView>(R.id.icon_balances),
-            findViewById<ImageView>(R.id.icon_agenda),
-            findViewById<ImageView>(R.id.icon_movements)
-        )
-        texts = listOf(
-            findViewById<TextView>(R.id.text_groups),
-            findViewById<TextView>(R.id.text_balances),
-            findViewById<TextView>(R.id.text_calendar),
-            findViewById<TextView>(R.id.text_movto)
-        )
         setupBottomNavigation()
     }
 
-
     private fun setupBottomNavigation() {
-        icons.forEach { icon ->
-            icon.setOnClickListener {
-                updateIconColors(icon)
-            }
+        icons[IDX_GROUPS].setOnClickListener {
+            super.updateIconColors(icons[IDX_GROUPS])
+            loadFragment(GroupsFragment())
+        }
+        icons[IDX_BALANCES].setOnClickListener {
+            super.updateIconColors(icons[IDX_BALANCES])
+            loadFragment(BalancesFragment())
+        }
+        icons[IDX_AGENDA].setOnClickListener {
+            super.updateIconColors(icons[IDX_AGENDA])
+            loadFragment(AgendaFragment())
+        }
+        icons[IDX_MOVEMENT].setOnClickListener {
+            super.updateIconColors(icons[IDX_MOVEMENT])
+            loadFragment(MovementFragment())
         }
     }
 
-    private fun updateIconColors(selectedIcon: ImageView) {
-        val disabledColor = ContextCompat.getColor(this, R.color.disabled_icon)
-        val enabledColor = ContextCompat.getColor(this, R.color.enabled_icon)
-        icons.forEachIndexed { idx, icon ->
-            val color = if (icon == selectedIcon) disabledColor else enabledColor
-            icon.setColorFilter(color)
-            val textView = texts[idx]
-            textView.setTextColor(color)
-        }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
+
 }
